e89a2a11ad6c447351ab134935f686d0fa249d79
Irvan Kadhafi <irvan.kadhavi@kumparan.com>
2021-12-10 20:44:17 +0700

    update button disable

--- app/resources/js/Pages/Home/Kandidat.vue
+++ app/resources/js/Pages/Home/Kandidat.vue
@@ -24,8 +24,9 @@
           </div>
         </div>
         <div class="px-4 pt-12 pb-4  flex items-center text-center leading-none text-gray-800">
-          <button class="btn-green m-auto" @click="toggleModalVisiMisi(data.id)">Visi & Misi</button>
-          <inertia-link class="btn-indigo float-right m-auto" as="button" :href="'/kandidat/resume/'+data.id">Dokumen CV</inertia-link>
+            <button class="m-auto" :class="(data.id===2)?'btn-disable cursor-not-allowed':'btn-green'" @click="toggleModalVisiMisi(data.id)" :disabled="data.id === 2">Visi & Misi</button>
+            <inertia-link class="float-right m-auto" :class="(data.id===2)?'btn-disable cursor-not-allowed':'btn-indigo'" as="button" :href="route('cv.kandidat',data.id)" :disabled="data.id === 2">Dokumen CV</inertia-link>
+
         </div>
       </div>
     </div>
