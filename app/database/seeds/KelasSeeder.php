493c54f153525818f15419dc009ef6cd7216524f
Irvan Kadhafi <irvankadhafi04@gmail.com>
2022-12-19 22:55:02 +0700

    update kelas seeder

--- app/database/seeds/KelasSeeder.php
+++ app/database/seeds/KelasSeeder.php
@@ -14,106 +14,120 @@ class KelasSeeder extends Seeder
      */
     public function run()
     {
+        // KELAS 1
         $kelas = new Kelas();
-        $kelas->name = "1A";
-        $kelas->status=0;
-        $kelas->id_prodi = 24401;
-        $kelas->save();
-
-
-        $kelas = new Kelas();
-        $kelas->name = "1B";
-        $kelas->id_prodi = "24401";
-        $kelas->status=0;
-        $kelas->save();
-
-        $kelas = new Kelas();
-        $kelas->name = "1C";
-        $kelas->id_prodi = "24401";
-        $kelas->status=0;
-        $kelas->save();
-
-        $kelas = new Kelas();
-        $kelas->name = "1D";
-        $kelas->id_prodi = "24301";
-        $kelas->status=0;
-        $kelas->save();
-
-        $kelas = new Kelas();
-        $kelas->name = "1E";
-        $kelas->id_prodi = "24402";
-        $kelas->status=0;
-        $kelas->save();
-
-        $kelas = new Kelas();
-        $kelas->name = "1F";
-        $kelas->id_prodi = "24402";
-        $kelas->status=0;
-        $kelas->save();
-
-        $kelas = new Kelas();
-        $kelas->name = "2A";
+        $kelas->name = "1A-TKI";
         $kelas->status=0;
         $kelas->id_prodi = 24401;
         $kelas->save();
 
         $kelas = new Kelas();
-        $kelas->name = "2B";
+        $kelas->name = "1B-TKI";
         $kelas->id_prodi = "24401";
         $kelas->status=0;
         $kelas->save();
 
         $kelas = new Kelas();
-        $kelas->name = "2C";
+        $kelas->name = "1C-TKI";
         $kelas->id_prodi = "24401";
         $kelas->status=0;
         $kelas->save();
 
         $kelas = new Kelas();
-        $kelas->name = "2D";
-        $kelas->id_prodi = "24301";
-        $kelas->status=0;
-        $kelas->save();
-
-        $kelas = new Kelas();
-        $kelas->name = "2E";
+        $kelas->name = "1A-ANK";
         $kelas->id_prodi = "24402";
         $kelas->status=0;
         $kelas->save();
 
         $kelas = new Kelas();
-        $kelas->name = "3A";
+        $kelas->name = "1B-ANK";
+        $kelas->id_prodi = "24402";
+        $kelas->status=0;
+        $kelas->save();
+
+        $kelas = new Kelas();
+        $kelas->name = "1A-TKPB";
+        $kelas->id_prodi = "24301";
+        $kelas->status=0;
+        $kelas->save();
+
+        $kelas = new Kelas();
+        $kelas->name = "1B-TKPB";
+        $kelas->id_prodi = "24301";
+        $kelas->status=0;
+        $kelas->save();
+
+        // KELAS 2
+        $kelas = new Kelas();
+        $kelas->name = "2A-TKI";
         $kelas->status=0;
         $kelas->id_prodi = 24401;
         $kelas->save();
 
-
         $kelas = new Kelas();
-        $kelas->name = "3B";
+        $kelas->name = "2B-TKI";
         $kelas->id_prodi = "24401";
         $kelas->status=0;
         $kelas->save();
 
         $kelas = new Kelas();
-        $kelas->name = "3C";
+        $kelas->name = "2C-TKI";
         $kelas->id_prodi = "24401";
         $kelas->status=0;
         $kelas->save();
 
         $kelas = new Kelas();
-        $kelas->name = "3D";
-        $kelas->id_prodi = "24301";
-        $kelas->status=0;
-        $kelas->save();
-
-        $kelas = new Kelas();
-        $kelas->name = "3E";
+        $kelas->name = "2A-ANK";
         $kelas->id_prodi = "24402";
         $kelas->status=0;
         $kelas->save();
 
         $kelas = new Kelas();
-        $kelas->name = "4D";
+        $kelas->name = "2B-ANK";
+        $kelas->id_prodi = "24402";
+        $kelas->status=0;
+        $kelas->save();
+
+        $kelas = new Kelas();
+        $kelas->name = "2-TKPB";
+        $kelas->id_prodi = "24301";
+        $kelas->status=0;
+        $kelas->save();
+
+        // KELAS 3
+        $kelas = new Kelas();
+        $kelas->name = "3A-TKI";
+        $kelas->status=0;
+        $kelas->id_prodi = 24401;
+        $kelas->save();
+
+        $kelas = new Kelas();
+        $kelas->name = "3B-TKI";
+        $kelas->id_prodi = "24401";
+        $kelas->status=0;
+        $kelas->save();
+
+        $kelas = new Kelas();
+        $kelas->name = "3C-TKI";
+        $kelas->id_prodi = "24401";
+        $kelas->status=0;
+        $kelas->save();
+
+        $kelas = new Kelas();
+        $kelas->name = "3-ANK";
+        $kelas->id_prodi = "24402";
+        $kelas->status=0;
+        $kelas->save();
+
+        $kelas = new Kelas();
+        $kelas->name = "3-TKPB";
+        $kelas->id_prodi = "24301";
+        $kelas->status=0;
+        $kelas->save();
+
+        // Kelas 4
+        $kelas = new Kelas();
+        $kelas->name = "4-TKPB";
         $kelas->id_prodi = "24301";
         $kelas->status=0;
         $kelas->save();
